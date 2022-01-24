2b14a1200cf81fe4dbfa947be8b7480ba8c04257
Kris Powell <kris.powell@kkr.com>
2022-01-24 20:43:40 +0100

    Working search.. but will I let it ruin wordle for me?

--- hint.py
+++ hint.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 from collections import Counter
+import itertools
+import functools
+import argparse
 
 
 def load_six_letter_words():
@@ -12,14 +15,12 @@ def count_unique_letter_frequencies(words):
     return Counter(letter for word in words for letter in set(word))
 
 
-
 class Dictionary:
 
     def __init__(self):
         words = load_six_letter_words()
         self.frequencies = count_unique_letter_frequencies(words)
         self.ordered_words = self.order_words_by_unique_letter_frequency(words)
-
 
     def order_words_by_unique_letter_frequency(self, words):
         return sorted(words, key=self.word_weight, reverse=True)
@@ -28,7 +29,55 @@ class Dictionary:
         return sum(self.frequencies[letter] for letter in set(word.lower().strip()))
 
 
-d = Dictionary()
-print(f"{len(d.ordered_words)} six letter words loaded")
-print(d.ordered_words[:100])
-print(f"train is {d.ordered_words.index('train') + 1}th word in list")
+def parse_args():
+    parser = argparse.ArgumentParser(description='Suggest next guesses..')
+    parser.add_argument('guesses', metavar='G', type=str, nargs='*',
+                        help='Guess/result pairs: '
+                        '"train/..xXx" means "a" and "n" are present but in different spot, "i" is in correct location')
+    return parser.parse_args()
+
+
+def next_best(guesses):
+    d = Dictionary()
+    if not guesses:
+        print(f"{len(d.ordered_words)} six letter words loaded.. Best starting words:")
+    return apply_constraints(d, guesses)
+
+
+def apply_constraints(dictionary, guesses):
+    constraints = tuple(create_constraints(guesses))
+    return (word for word in dictionary.ordered_words if all(match(word) for match in constraints))
+
+
+def create_constraints(guesses):
+    for guess_and_result in guesses:
+        guess, result = guess_and_result.split('/')
+        assert len(guess) == len(result) == 5, 'guess and result must be 5 long'
+        guess = guess.lower()
+        for position, (letter, result) in enumerate(zip(guess, result)):
+            if result == '.':
+                yield functools.partial(no_match, position, letter)
+            elif result == 'X':
+                yield functools.partial(exact_match, position, letter)
+            elif result == 'x':
+                yield functools.partial(inexact_match, position, letter)
+            else:
+                raise ValueError(f"Invalid result '#{result}': must be one of '.xX'")
+
+
+def no_match(position, letter, candidate):
+    return letter not in candidate
+
+
+def exact_match(position, letter, candidate):
+    return candidate[position] == letter
+
+
+def inexact_match(position, letter, candidate):
+    return letter in candidate and candidate[position] != letter
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    candidates = next_best(args.guesses)
+    print(', '.join(itertools.islice(candidates, 10)))
